406cafb89246b8257e73d5859b50364a4f0a1cfc
vladaliho <vlada.liho@gmail.com>
2024-10-08 17:17:31 +0100

    feat: add func

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.currentGuess = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if (this.min === null || this.max === null) {
+            throw new Error('Range is not set.');
+        }
+        this.currentGuess = Math.floor((this.min + this.max) / 2); 
+        return this.currentGuess;
     }
 
     lower() {
-
+        if (this.currentGuess !== null) {
+            this.max = this.currentGuess; 
+        }
     }
 
     greater() {
-
+        if (this.currentGuess !== null) {
+            this.min = this.currentGuess + 1; 
+        }
     }
 }
 

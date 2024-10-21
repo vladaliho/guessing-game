ad2ab39f61a82e541984057f9f49ae416ee80397
vladaliho <vlada.liho@gmail.com>
2024-10-21 19:17:56 +0100

    fix: fixed the func

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,34 +1,27 @@
 class GuessingGame {
-    constructor() {
-        this.min = null;
-        this.max = null;
-        this.currentGuess = null;
-    }
+	constructor() {
+		this.min = null;
+		this.max = null;
+		this.currentGuess = null;
+	}
 
-    setRange(min, max) {
-        this.min = min;
-        this.max = max;
-    }
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
 
-    guess() {
-        if (this.min === null || this.max === null) {
-            throw new Error('Range is not set.');
-        }
-        this.currentGuess = Math.floor((this.min + this.max) / 2); 
-        return this.currentGuess;
-    }
+	guess() {
+		this.currentGuess = Math.ceil((this.min + this.max) / 2);
+		return this.currentGuess;
+	}
 
-    lower() {
-        if (this.currentGuess !== null) {
-            this.max = this.currentGuess; 
-        }
-    }
+	lower() {
+		this.max = this.currentGuess;
+	}
 
-    greater() {
-        if (this.currentGuess !== null) {
-            this.min = this.currentGuess + 1; 
-        }
-    }
+	greater() {
+		this.min = this.currentGuess;
+	}
 }
 
 module.exports = GuessingGame;
